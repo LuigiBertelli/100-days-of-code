f2952d0db142220fb9544d77bf9c6f720e29dcd1
Luigi Bertelli <berteluigi@gmail.com>
2023-01-05 12:12:43 +0100

    2nd commit - 05/01/2023

---
+++ Main/Week1/Day5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Day5 {
+
+    public class Sock {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static int SockPairs(string socks) {
+
+            int countPairs = 0;
+            char[] socksChars = socks.ToCharArray();
+            
+            while(socksChars.Length > 0)
+            {
+                char actualChar = socksChars[0];
+                countPairs += (int) Math.Floor(socks.Count(x => x == actualChar)  / 2.0d);
+                socksChars = socksChars.Where(x => x != actualChar).ToArray();
+            }
+
+            return countPairs;
+        }
+    } 
+}
