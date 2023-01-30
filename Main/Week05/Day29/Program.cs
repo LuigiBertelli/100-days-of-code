8985eb612f3789ad5741a72790e15c85c7fd5b13
Luigi Bertelli <berteluigi@gmail.com>
2023-01-30 15:56:25 +0100

    Commit Challenge day 29 - 30/01/2023

---
+++ Main/Week05/Day29/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day29
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = (string?) Console.ReadLine();
+
+            var splittedInput = input?.ToUpper().Split(' ') ?? new string[0];
+
+            var allowedLetters = new string[] {"R", "G", "Y", "P", "C", "X"};
+
+            if(splittedInput.Length > 0 && splittedInput.Length <= 15 && splittedInput.Where(x => !allowedLetters.Contains(x)).Count() == 0 ) {
+                string lastLetter = string.Empty;
+                bool accept = true;
+                if(splittedInput[0] == "R") {
+                    foreach(var state in splittedInput) {
+                        if(!string.IsNullOrEmpty(lastLetter)) {
+                            switch(lastLetter) {
+                                case "R": 
+                                    if(state == "R" || state == "Y")
+                                        accept = false;
+                                    break;
+                                case "G": 
+                                    if(state == "G" || state == "R")
+                                        accept = false;
+                                    break;
+                                case "Y": 
+                                    if(state == "Y" || state == "G")
+                                        accept = false;
+                                    break;
+                                case "X": 
+                                    accept = false;
+                                    break;
+                            }
+                        }
+
+                        lastLetter = state;
+
+                        if(!accept)
+                            break;
+                    }
+                } else
+                    accept = false;
+
+                if(accept)
+                    Console.WriteLine("Accept");
+                else
+                    Console.WriteLine("Reject");
+            } else
+                Console.WriteLine("Error");
+        }
+    }
+}
